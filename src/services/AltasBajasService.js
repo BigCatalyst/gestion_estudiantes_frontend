663cbfd659a4f1092ef0501f4f848dc4297d6b4c
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-08-30 22:51:19 -0400

    altas bajas servicios

--- src/services/AltasBajasService.js
+++ src/services/AltasBajasService.js
@@ -20,34 +20,76 @@ let rows = [
   },
 ];
 
-// export const getAll = async () => rows;
 export const getAll = async () => axios.get("/AltasBajas/findAll", axiosCong());
-export const add = (data) => {
-  rows = [...rows, { ...data }];
-  //console.log(rows);
-  return true;
-};
-export const update = (data) => {
-  console.log(data);
-  const updatedRows = rows.map((row) => {
-    //console.log(row.username, data.username);
-    if (row.ci === data.ci) {
-      return { ...data };
+export const add =  async (data) => axios.post("/AltasBajas/create",data, axiosCong());
+export const update = async (data) => axios.put("/AltasBajas/update",data, axiosCong());
+export const changepassword = async (data) => axios.put("/AltasBajas/changepassword",data, axiosCong());
+export const remove =  async (id) => axios.delete(`/AltasBajas/changepassword/${id}`, axiosCong());
+export const findid =  async (id) => axios.get(`/AltasBajas/find/${id}`, axiosCong());
+
+// export const reporte = async () => axios.get("/Users/reporte", axiosCong());
+
+export const reporte =  async () =>{
+  try{
+    const res=await axios.get(
+      "/AltasBajas/reporte", 
+    {
+      responseType: "blob",
+      ...axiosCong()
+    });
+  
+    if (res.status==200) {
+      console.log("respuesta");
+      console.log(res);
+      const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+      const pdfUrl = URL.createObjectURL(pdfBlob);
+  
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.download = "reporte.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }else{
+      console.log("no fue 200"+res.status);
     }
-    return { ...row };
-  });
 
-  rows = updatedRows;
+  }catch(error){
+    console.log(error);
+  }
+  
 
-  return true;
 };
-export const remove = (ci) => {
-  const index = rows.findIndex((el) => el.username === ci);
-  console.log(index);
-  let cop = [];
-  rows.map((el) => {
-    if (el.ci != ci) return cop.push(el);
-  });
-  rows = [...cop];
-  return true;
+
+export const reportemensual =  async (year,mes) =>{
+  try{
+    const res=await axios.get(
+      `/AltasBajas/reporte/pormes/${year}/${mes}`, 
+    {
+      responseType: "blob",
+      ...axiosCong()
+    });
+  
+    if (res.status==200) {
+      console.log("respuesta");
+      console.log(res);
+      const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+      const pdfUrl = URL.createObjectURL(pdfBlob);
+  
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.download = "reporte.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }else{
+      console.log("no fue 200"+res.status);
+    }
+
+  }catch(error){
+    console.log(error);
+  }
+  
+
 };
+

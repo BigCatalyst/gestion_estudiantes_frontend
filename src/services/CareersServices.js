320b7645939f983958e815b6f1dc510a4547e351
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-08-30 22:39:51 -0400

    servicio carreras

--- src/services/CareersServices.js
+++ src/services/CareersServices.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { axiosCong } from "../config/env";
+
 let rows = [
   {
     id: 1,
@@ -11,32 +14,36 @@ let rows = [
   },
 ];
 
-export const getAll = async () => rows;
-export const add = (data) => {
-  let id = rows[rows.length - 1].id + 1;
-  rows = [...rows, { ...data, id }];
-  //console.log(rows);
-  return true;
-};
-export const update = (data) => {
-  console.log(data);
-  const updatedRows = rows.map((row) => {
-    //console.log(row.username, data.username);
-    if (row.id === data.id) {
-      return { ...data };
+export const getAll = async () => axios.get("/Careers/findAll", axiosCong());
+export const add = async (data) => axios.post("/Careers/create",data, axiosCong());
+export const update = async (data) => axios.put("/Careers/update",data, axiosCong());
+export const remove =  async (id) => axios.delete(`/Careers/changepassword/${id}`, axiosCong());
+export const reporte =  async () =>{
+  try{
+    const res=await axios.get(
+      "/Careers/reporte", 
+    {
+      responseType: "blob",
+      ...axiosCong()
+    });
+  
+    if (res.status==200) {
+      console.log("respuesta");
+      console.log(res);
+      const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+      const pdfUrl = URL.createObjectURL(pdfBlob);
+  
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.download = "reporte.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }else{
+      console.log("no fue 200"+res.status);
     }
-    return { ...row };
-  });
 
-  rows = updatedRows;
-
-  return true;
-};
-export const remove = (id) => {
-  let cop = [];
-  rows.map((el) => {
-    if (el.id != id) return cop.push(el);
-  });
-  rows = [...cop];
-  return true;
-};
+  }catch(error){
+    console.log(error);
+  }
+}

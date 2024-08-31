32090b3ea1e60b352c49505798478353df7d15f5
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-08-31 00:41:39 -0400

    graduado

--- src/pages/graduate/Graduate.jsx
+++ src/pages/graduate/Graduate.jsx
@@ -1,7 +1,181 @@
+import { useState } from "react";
 import Page from "../../components/page/Page";
-
+import { useEffect } from "react";
+import { getAll } from "../../services/GraduateService";
+import { Box, Button } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { dataGridStyles } from "../../components/mui/datagrid/DataGridStyle";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 const Graduate = () => {
-  return <Page title="Graduados"></Page>;
+  //const [request, setRequest] = useState(0);
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [keyDataGrid, setKeyDataGrid] = useState(Date.now());
+
+  
+
+  useEffect(() => {
+    //simulando el tiempo de respuesta de la api
+    const getData = async () => {
+      setLoading(true);
+      let count = 0;
+      const res = await getAll();
+      if (res) {
+        const dataRequest = res.data;
+        const dataT = dataRequest.map((item) => {
+          count++;
+          return { ...item, id: count };
+        });
+        setData(dataT);
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [keyDataGrid]);
+
+  
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 150 },
+    {
+      field: "ci",
+      headerName: "CI",
+      width: 150,
+    },
+    {
+      field: "direccion",
+      headerName: "Address",
+      width: 150,
+    },
+    {
+      field: "grade",
+      headerName: "Grado",
+      width: 150,
+    },
+    {
+      field: "apellidos",
+      headerName: "Apellidos",
+      width: 150,
+    },
+    {
+      field: "nombre",
+      headerName: "Nombre",
+      width: 150,
+    },
+    {
+      field: "nodematricula",
+      headerName: "No Matricula",
+      width: 150,
+    },
+    {
+      field: "sexo",
+      headerName: "Genero",
+      width: 150,
+    },
+    { field:"fechagraduacion",
+      headerName:"Graduacion",
+      width: 150,
+     },
+     { field:"carrera",
+      headerName:"Carrera",
+      width: 150,
+     },
+     { field:"notaescalafon",
+      headerName:"Nota Escalafon",
+      width: 150,
+     },
+  ];
+
+  return (
+    <Page title="Graduado">
+      <Box>
+        <Grid container spacing={3} sx={{ mt: 1 }}>
+          {/* <Grid
+            xs={12}
+            display="flex"
+            justifyContent="flex-end"
+            sx={{ mx: 10 }}
+          >
+            <Button
+              variant="contained"
+              startIcon={<BsPersonFillAdd />}
+              onClick={handleOpenAddM}
+            >
+              Agregar
+            </Button>
+            <ModalMUI
+              open={openAddM}
+              handleClose={handleCloseAddM}
+              title="Adicionar Usuario"
+            >
+              <AddUser
+                setKeyDataGrid={setKeyDataGrid}
+                handleCloseAddM={handleCloseAddM}
+              />
+            </ModalMUI>
+            <ModalMUI
+              open={openUpdM}
+              handleClose={handleCloseUpdM}
+              title="Actualizar Usuario"
+            >
+              <UpdateUser
+                setKeyDataGrid={setKeyDataGrid}
+                handleCloseUpdM={handleCloseUpdM}
+                dataEdit={dataEdit}
+              />
+            </ModalMUI>
+            <ModalMUI
+              open={openDelM}
+              handleClose={handleCloseDelM}
+              title="Eliminar Usuario"
+            >
+              <DeleteUser
+                setKeyDataGrid={setKeyDataGrid}
+                handleCloseDelM={handleCloseDelM}
+                dataDel={dataDel}
+              />
+            </ModalMUI>
+          </Grid> */}
+          
+          <Grid xs={12}>
+            <DataGrid
+              key={keyDataGrid}
+              rows={data}
+              columns={columns}
+              columnVisibilityModel={{
+                //id: false,
+                password: false,
+              }}
+              initialState={{
+                dataSet: "Commodity",
+                maxColumns: 6,
+                pagination: { paginationModel: { pageSize: 5 } },
+                sorting: {
+                  sortModel: [{ field: "id", sort: "desc" }],
+                },
+              }}
+              pageSizeOptions={[5, 10, 25]}
+              sx={dataGridStyles}
+              loading={loading}
+              disableDensitySelector
+              disableColumnSelector
+              slotProps={{
+                toolbar: {
+                  csvOptions: { disableToolbarButton: true },
+                  printOptions: { disableToolbarButton: true },
+                  // Agrega más opciones de exportación si es necesario
+                },
+              }}
+              slots={{
+                toolbar: GridToolbar,
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </Page>
+  );
 };
 
 export default Graduate;
